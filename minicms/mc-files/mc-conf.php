8c59eeff68480bad4e14035f36c2f85c47216fcf
nekoCCC <38419157+nekoCCC@users.noreply.github.com>
2018-11-14 00:04:42 +0800

    Add files via upload

---
+++ minicms/mc-files/mc-conf.php
@@ -0,0 +1,1 @@
+<?php $mc_config = array('version'   => '/*MINICMS_VERSION*/','site_link' => 'http://localhost/minicms','site_name' => 'minicms漏洞复现','site_desc' => '又一个MiniCMS网站','user_name' => 'admin','user_pass' => '123456','user_nick' => '神秘人','comment_code' => '');?>
---
+++ minicms/mc-files/posts/index/publish.php
@@ -0,0 +1,16 @@
+<?php
+$mc_posts=array (
+  'obk3ek' => 
+  array (
+    'id' => 'obk3ek',
+    'state' => 'publish',
+    'title' => '这是一篇测试文章',
+    'tags' => 
+    array (
+    ),
+    'date' => '2018-11-13',
+    'time' => '23:07:00',
+    'can_comment' => '0',
+  ),
+)
+?>
